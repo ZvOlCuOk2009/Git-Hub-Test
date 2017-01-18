002ca86212d37594a243ec75011ed2872168d3b4
Zvigun Alexandr <mircamnia@ukr.net>
2017-01-18 15:15:26 +0200

    Initial Commit

---
+++ Git Hub Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Hub Test
+//
+//  Created by Mac on 18.01.17.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Hub Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Hub Test
+//
+//  Created by Mac on 18.01.17.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
